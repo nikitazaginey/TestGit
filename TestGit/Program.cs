e5bb92fe7c6184aa471a348a3833aa8f1a9215a3
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-05 16:50:35 +0200

    Add project files.

---
+++ TestGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = "example 1";
+            string line2 = "example 2";
+            string line3 = "example 3";
+        }
+    }
+}
