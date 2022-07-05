5fe71e654c792b19a2a2978e6759689dd82a9353
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-05 17:02:57 +0200

    Change the second line

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,7 +7,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             string line1 = "example 1";
-            string line2 = "example 2";
+            string line2 = "Text exapmle";
             string line3 = "example 3";
             string line4 = "example 4";
         }
